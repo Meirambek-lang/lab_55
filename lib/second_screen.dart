807b1b8bdb9437444976fe154440b97d246178ab
Meirambek-lang <mikokaribaev272@gmail.com>
2025-03-14 10:26:10 +0500

    Update second_screen.dart

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
+// Второй экран приложения
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Second Screen')),
+      appBar: AppBar(title: Text('Second Screen')), // Верхняя панель с заголовком
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center, // Центрируем элементы по вертикали
         children: [
+          // Кнопка для возврата на предыдущий экран
           ElevatedButton(
             onPressed: () {
               Navigator.pop(context);
@@ -17,6 +19,7 @@ class SecondScreen extends StatelessWidget {
             child: Text('Pop'),
           ),
           SizedBox(height: 20), // Отступ между кнопками
+          // Кнопка для закрытия текущего экрана и перехода на third
           ElevatedButton(
             onPressed: () {
               Navigator.popAndPushNamed(context, '/third');
